b31ce0251e8a5de159034416654b99917ee9da1d
mfathony115 <mfathony115@gmail.com>
2025-01-06 13:48:12 +0700

    new with slider

--- src/pages/productDetail/index.jsx
+++ src/pages/productDetail/index.jsx
@@ -1,5 +1,9 @@
-import { lazy, Suspense, useRef, useEffect } from "react"
+import { lazy, Suspense, useRef, useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
+import { SwiperSlide, Swiper } from 'swiper/react'
+import { Pagination, Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 import product1 from "../../assets/img/product/product-1.jpg"
 import product2 from "../../assets/img/product/product-2.jpg"
@@ -51,8 +55,63 @@ const products = [
 
 const Product = lazy(() => import("../../components/products/item"))
 const Instagram = lazy(() => import("../../components/instagram"))
-
+const details = [
+    {
+        id: 1,
+        img: productDetail3,
+    },
+    {
+        id: 2,
+        img: productDetail1,
+    },
+    {
+        id: 3,
+        img: productDetail2,
+    },
+    {
+        id: 4,
+        img: productDetail4,
+    }
+]
+const thumbDetails = [
+    {
+        id: 1,
+        img: detail1,
+    },
+    {
+        id: 2,
+        img: detail2,
+    },
+    {
+        id: 3,
+        img: detail3,
+    },
+    {
+        id: 4,
+        img: detail4,
+    }
+]
 const Component = () => {
+    const swiperRef = useRef(null);
+    const swiperConfig = {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        modules: [Pagination, FreeMode, Navigation, Thumbs],
+        navigation: true,
+        slidesPerView:1
+    };
+
+    const handleNavigationClick = (index) => {
+        if (swiperRef.current) {
+            console.log(index)
+            swiperRef.current.slideTo(index); // Pindah ke slide tertentu
+        }
+    };
+
     return (
         <>
             <section className="product-details spad">
@@ -61,26 +120,42 @@ const Component = () => {
                         <div className="col-lg-6">
                             <div className="product__details__pic">
                                 <div className="product__details__pic__left product__thumb nice-scroll">
-                                    <a className="pt active" href="#product-1">
-                                        <img src={detail1} alt="icon-detail.jpeg" />
-                                    </a>
-                                    <a className="pt" href="#product-2">
-                                        <img src={detail2} alt="icon-detail.jpeg" />
-                                    </a>
-                                    <a className="pt" href="#product-3">
-                                        <img src={detail3} alt="icon-detail.jpeg" />
-                                    </a>
-                                    <a className="pt" href="#product-4">
-                                        <img src={detail4} alt="icon-detail.jpeg" />
-                                    </a>
+                                    {
+                                        thumbDetails.map((value, key) => (
+                                            <a
+                                                className={`pt ${swiperRef.current?.activeIndex === key ? 'active' : ''}`}
+                                                href="#"
+                                                key={key}
+                                                onClick={(e) => {
+                                                    e.preventDefault(); // Mencegah reload halaman
+                                                    handleNavigationClick(key);
+                                                }}
+                                            >
+                                                <img src={value.img} alt="icon-detail.jpeg" />
+                                            </a>
+                                        ))
+                                    }
                                 </div>
                                 <div className="product__details__slider__content">
-                                    <div className="product__details__pic__slider owl-carousel">
-                                        <img data-hash="product-1" className="product__big__img" src={productDetail1} alt="detail.jpeg" />
-                                        {/* <img data-hash="product-2" className="product__big__img" src={productDetail2} alt="detail.jpeg" />
-                                        <img data-hash="product-3" className="product__big__img" src={productDetail3} alt="detail.jpeg" />
-                                        <img data-hash="product-4" className="product__big__img" src={productDetail4} alt="detail.jpeg" /> */}
-                                    </div>
+                                    <Swiper
+                                        onSwiper={(swiper) => (swiperRef.current = swiper)}
+                                        className="product_details_pic_slider"
+                                        {...swiperConfig}
+                                    >
+                                        {
+                                            details.map((value, index) => (
+                                                <SwiperSlide key={index}>
+                                                    <img
+                                                        width="100%"
+                                                        className="product__big__img img-fluid"
+                                                        src={value.img}
+                                                        alt={`detail-${index + 1}`}
+                                                    />
+                                                </SwiperSlide>
+                                            ))
+                                        }
+                                        <div className="swiper-pagination"></div>
+                                    </Swiper>
                                 </div>
                             </div>
                         </div>
